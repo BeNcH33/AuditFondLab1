97495eb8473f470b61a176bccaa8296bb981e1ac
slava <slava@BENCH-DESKTOP>
2021-10-06 13:43:58 +0300

    Добавьте файлы проекта.

---
+++ AuditFondLab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AuditFondLab1
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Audit audit = new Audit();
+        //    Console.WriteLine("1");
+        //    audit.addRoom(Console.ReadLine());
+        //    Console.WriteLine("2");
+        //    //audit.addTeacher(Console.ReadLine());
+        //}
+    }
+}
